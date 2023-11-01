692936e5bbb06ed1ba8a544eac5668f8c3bfecb9
Matias Garafoni <matias.garafoni@gmail.com>
2023-11-01 13:49:04 -0300

    initialize services with os environ vars from docker-compose

--- src/process.py
+++ src/process.py
@@ -34,11 +34,11 @@ class ServiceProcessor(BaseServiceProcessor):
         self.aws_region = aws_region
         self.sqs_queue_url = sqs_queue_url
         try:
-            # Start listening to SQS simultaneously
+            # Start listening to SQS
             import concurrent.futures
             with concurrent.futures.ThreadPoolExecutor() as executor:
                 executor.submit(self.listen_sqs)
-        except KeyboardInterrupt:
+        except Exception:
             logging.info('Service interrupted. Exiting...')
 
     def callback_rabbitmq(self,ch, method, properties, body):
@@ -56,16 +56,50 @@ class ServiceProcessor(BaseServiceProcessor):
         channel.queue_declare(queue=self.rabbitmq_queue_name)
         channel.basic_consume(queue=self.rabbitmq_queue_name, on_message_callback=self.callback_rabbitmq, auto_ack=True)
 
-        logging.info('Waiting for messages from RabbitMQ. To exit press CTRL+C')
+        logging.info('Waiting for messages from RabbitMQ.')
         channel.start_consuming()
 
     def listen_sqs(self):   
         sqs = boto3.resource('sqs', region_name=self.aws_region, aws_access_key_id=self.aws_access_key, aws_secret_access_key=self.aws_secret_key)
         queue = sqs.Queue(self.sqs_queue_url)
 
-        logging.info('Waiting for messages from SQS. To exit press CTRL+C')
+        logging.info('Waiting for messages from SQS.')
         while True:
             messages = queue.receive_messages(WaitTimeSeconds=20)
             for message in messages:
                 self.callback_sqs(message)
                 message.delete()
+
+
+
+if __name__ == '__main__':
+
+    import os
+
+    rabbitmq_host = os.environ.get('RABBITMQ_HOST')
+    rabbitmq_queue = os.environ.get('RABBITMQ_QUEUE')
+
+    aws_access_key = os.environ.get('AWS_ACCESS_KEY')
+    aws_secret_key = os.environ.get('AWS_SECRET_KEY')
+    aws_region = os.environ.get('AWS_REGION')
+    sqs_queue_url = os.environ.get('SQS_QUEUE_URL')
+
+    start_rabbitmq = os.environ.get("RABBITMQ_START")
+    start_sqs = os.environ.get("SQS_START")
+
+    a_service_started = False
+
+    if start_rabbitmq == "START":
+        # Create instances of your ServiceProcessor class and use the configuration parameters
+        rabbitmq_service_processor = ServiceProcessor()
+        rabbitmq_service_processor.start_rabbit_mq_processor(rabbitmq_host, rabbitmq_queue)
+        a_service_started = True
+
+    if start_sqs == "START":
+        # Create instances of your ServiceProcessor class and use the configuration parameters
+        sqs_service_processor = ServiceProcessor()
+        sqs_service_processor.start_sqs_processor(aws_access_key, aws_secret_key, aws_region, sqs_queue_url)
+        a_service_started = True
+
+    if not a_service_started:
+        logging.warning("No service was set to start.")
