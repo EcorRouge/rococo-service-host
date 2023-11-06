c52b0b68a99aa24b969c158a26fb8cf23339f623
Matias Garafoni <matias.garafoni@gmail.com>
2023-11-06 13:55:28 -0300

    rename env vars here

--- tests/test_message.py
+++ tests/test_message.py
@@ -7,12 +7,12 @@ def test_rabbitmq_send_message():
     # Establish a connection to RabbitMQ server
 
     credentials = pika.PlainCredentials(
-        username=os.environ.get("RABBITMQ_DEFAULT_USER"),
-        password=os.environ.get("RABBITMQ_DEFAULT_PASS")
+        username=os.environ.get("USERNAME"),
+        password=os.environ.get("PASSWORD")
     )
     parameters = pika.ConnectionParameters(
-        host=os.environ.get("RABBITMQ_HOST"),
-        port=int(os.environ.get("RABBITMQ_PORT")),
+        host=os.environ.get("HOST"),
+        port=int(os.environ.get("PORT")),
         virtual_host=os.environ.get("RABBITMQ_VIRTUAL_HOST"),
         credentials=credentials
     )
@@ -22,11 +22,11 @@ def test_rabbitmq_send_message():
     channel = connection.channel()
 
     # Declare a queue named 'test_queue'
-    channel.queue_declare(queue=os.environ.get("RABBITMQ_QUEUE"), durable=True)
+    channel.queue_declare(queue=os.environ.get("QUEUE_NAME"), durable=True)
 
     # Publish a message to the queue
     channel.basic_publish(exchange='',
-                          routing_key=os.environ.get("RABBITMQ_QUEUE"),
+                          routing_key=os.environ.get("QUEUE_NAME"),
                           body=json.dumps({"message": "Hello, RabbitMQ!"}))
 
     print("Message sent to RabbitMQ.")
