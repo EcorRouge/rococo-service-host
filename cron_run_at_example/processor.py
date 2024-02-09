459088260ec4a8e37b0012b9194e729d867160a5
Matias Garafoni <matias.garafoni@gmail.com>
2024-02-09 16:47:27 -0300

    cron "run at" example

---
+++ cron_run_at_example/processor.py
@@ -0,0 +1,21 @@
+"""
+Base example of a service processor
+"""
+
+import datetime
+from logger import Logger
+
+logger = Logger().get_logger()
+
+# This is an example implementation of a processor class that is fired by crontab.
+# This should be done in the child image
+class LoggingServiceProcessor():  # pylint: disable=R0903
+    """
+    Service processor that logs messages
+    """
+    def __init__(self):
+        pass
+
+    def process(self):
+        """Main processor loop"""
+        logger.info("Cron processor execution started at %s ...",datetime.datetime.utcnow())
