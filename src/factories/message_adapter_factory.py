ae5525713c0326e8077f93a2c297e1f59b1a3b9f
asymness <asym1999@gmail.com>
2024-04-05 02:57:23 +0500

    Add support for SQS messaging adapter

--- src/factories/message_adapter_factory.py
+++ src/factories/message_adapter_factory.py
@@ -4,6 +4,7 @@ Message adapter handling
 
 from rococo.messaging.base import MessageAdapter
 from rococo.messaging.rabbitmq import RabbitMqConnection
+from rococo.messaging.sqs import SqsConnection
 from .config_factory import Config
 
 
@@ -13,6 +14,9 @@ def get_message_adapter(config: Config) -> MessageAdapter:
     """
     if config.messaging_type == "RabbitMqConnection":
         adapter = RabbitMqConnection(*config.messaging_constructor_params)
+        return adapter
+    elif config.messaging_type == "SqsConnection":
+        adapter = SqsConnection(*config.messaging_constructor_params)
         return adapter
 
     return MessageAdapter()
